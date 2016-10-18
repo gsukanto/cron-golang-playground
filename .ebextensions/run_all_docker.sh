800aecb03d83109480de4c2745543f851ba51e3a
gsukanto <gregory.sukanto@gmail.com>
2016-10-18 10:20:12 +0700

    changes on the cron shell script (#5)

--- .ebextensions/run_all_docker.sh
+++ .ebextensions/run_all_docker.sh
@@ -1,3 +1,6 @@
-docker run aws_beanstalk/staging-app go run email_recipient_main.go &&
-docker run aws_beanstalk/staging-app go run low_inventory_main.go &&
-docker run aws_beanstalk/staging-app go run daily_sales_main.go
+echo "run get all email recipients at `date`" >> /tmp/golang.log
+docker run aws_beanstalk/staging-app go run email_recipient_main.go >> /tmp/golang.log
+echo "run low inventory at `date`" >> /tmp/golang.log
+docker run aws_beanstalk/staging-app go run low_inventory_main.go >> /tmp/golang.log
+echo "run daily sales at `date`" >> /tmp/golang.log
+docker run aws_beanstalk/staging-app go run daily_sales_main.go >> /tmp/golang.log
